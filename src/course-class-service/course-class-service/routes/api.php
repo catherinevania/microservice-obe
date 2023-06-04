b05dccb63f9962f7d483a5983a5a5acda8f8cd44
Amelia Kartika Wijaya <ameliakw@student.ub.ac.id>
2023-06-04 19:29:32 +0700

    add auth student teacher

--- src/course-class-service/course-class-service/routes/api.php
+++ src/course-class-service/course-class-service/routes/api.php
@@ -8,9 +8,30 @@ use App\Http\Controllers\CourseClassControllerStore;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+// Student Role
+// Route::group(['middleware' => ['roles:student']], function () {
+// 	Route::post('/course-class/{id}/add-student', [CourseClassController::class, 'addStudentToClass']);
+//     Route::get('/course-class/{id}/view-students', [CourseClassController::class, 'viewStudentInClass']);
+//     Route::get('/course', [CourseController::class, 'index']);
+//     Route::get('/course/{id}', [CourseController::class, 'show']);
+//     Route::get('/course-class', [CourseClassController::class, 'index']);
+//     Route::get('/course-class/{id}', [CourseClassController::class, 'show']);
+// });
 
-// Route::group(['middleware' => ['auth:sanctum']], function () {
-// 	Route::apiResource('users', UserController::class);
+// Teacher & Mimin Role
+// Route::group(['middleware' => ['roles:teacher']], function () {
+// 	Route::post('/course-class/{id}/add-student', [CourseClassController::class, 'addStudentToClass']);
+//     Route::get('/course-class/{id}/view-students', [CourseClassController::class, 'viewStudentInClass']);
+//     Route::get('/course', [CourseController::class, 'index']);
+//     Route::get('/course/{id}', [CourseController::class, 'show']);
+//     Route::post('/course', [CourseController::class, 'store']);
+//     Route::patch('/course/{id}', [CourseController::class, 'update']);
+//     Route::delete('/course/{id}', [CourseController::class, 'destroy']);
+//     Route::get('/course-class', [CourseClassController::class, 'index']);
+//     Route::get('/course-class/{id}', [CourseClassController::class, 'show']);
+//     Route::post('/course-class', [CourseClassController::class, 'store']);
+//     Route::patch('/course-class/{id}', [CourseClassController::class, 'update']);
+//     Route::delete('/course-class/{id}', [CourseClassController::class, 'destroy']);
 // });
 
 Route::apiResource('course', CourseController::class);
@@ -18,5 +39,3 @@ Route::apiResource('course-class', CourseClassController::class);
 
 Route::post('/course-class/{id}/add-student', [CourseClassController::class, 'addStudentToClass']);
 Route::get('/course-class/{id}/view-students', [CourseClassController::class, 'viewStudentInClass']);
-
-
