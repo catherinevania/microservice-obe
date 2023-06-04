b480fadd0b7e744cf749b291475bff1077784e6c
aufaazz <aufa.azmrn@gmail.com>
2023-06-05 06:46:01 +0700

    [ADD] CourseClassControllerTest.php at unit test

---
+++ src/course-class-service/course-class-service/tests/Unit/CourseClassControllerTest.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Tests\Unit;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use App\Models\CourseClass;
+
+
+class CourseClassControllerTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+
+    public function testCourseClassCanBeCreated()
+    {
+        $courseClassData = [
+            'id' => 1,
+            'course_id' => 1,
+            'name' => 'A',
+            'thumbnail_img' => 'image.png',
+            'class_code' => 'A123',
+            'creator_user_id' => 2,
+            'syllabus_id' => 1
+        ];
+ 
+         $courseClass = CourseClass::create($courseClassData);
+ 
+         $this->assertInstanceOf(CourseClass::class, $courseClass);
+         $this->assertEquals('A', $courseClass->name);
+         $this->assertEquals('A123', $courseClass->class_code);
+     }
+
+     public function testCourseClassNameIsRequired()
+    {
+        $courseClassData = [
+            'id' => 1,
+            'course_id' => 1,
+            'thumbnail_img' => 'image.png',
+            'class_code' => 'A123',
+            'creator_user_id' => 2,
+            'syllabus_id' => 1
+        ];
+
+        $this->expectException(\Illuminate\Database\QueryException::class);
+
+        CourseClass::create($courseClassData);
+    }
+
+
+    public function testCourseClassThumbnailImageIsAString()
+    {
+        $courseClassData = [
+            'id' => 1,
+            'course_id' => 1,
+            'name' => 'A',
+            'thumbnail_img' => 'image.png',
+            'class_code' => 'A123',
+            'creator_user_id' => 2,
+            'syllabus_id' => 1
+        ];
+
+        $courseClass = CourseClass::create($courseClassData);
+
+        $this->assertIsString($courseClass->thumbnail_img);
+    }
+
+}
+			
