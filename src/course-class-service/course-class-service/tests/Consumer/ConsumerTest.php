bad2bcda3e553ed95f51ddc6a48f490aa7107b5d
Amelia Kartika Wijaya <ameliakw@student.ub.ac.id>
2023-06-09 19:21:15 +0700

    configure ConsumerTest and HttpClientService

--- src/course-class-service/course-class-service/tests/Consumer/ConsumerTest.php
+++ src/course-class-service/course-class-service/tests/Consumer/ConsumerTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Consumer\Service;
+namespace Tests\Consumer;
 
 use PhpPact\Consumer\InteractionBuilder;
 use PhpPact\Consumer\Matcher\Matcher;
@@ -8,86 +8,48 @@ use PhpPact\Consumer\Model\ConsumerRequest;
 use PhpPact\Consumer\Model\ProviderResponse;
 use PhpPact\Standalone\MockService\MockServerEnvConfig;
 use PHPUnit\Framework\TestCase;
-use PhpPact\Standalone\MockService\MockServerConfig;
-use PhpPact\Standalone\MockService\MockServer;
-use PhpPact\Consumer\ConsumerBuilder;
+use App\OutsideAPI\HttpClientService;
 
-class ConsumerServiceHelloTest extends TestCase
+class ConsumerTest extends TestCase
 {
-	/**
-	 * Example PACT test.
-	 *
-	 * @throws \Exception
-	 */
+    /**
+     * Example PACT test.
+     *
+     * @throws \Exception
+     */
+    public function testGetHelloString()
+    {
+        $matcher = new Matcher();
 
-	protected static $pactMockServer;
+        // Create your expected request from the consumer.
+        $request = new ConsumerRequest();
+        $request
+            ->setMethod('GET')
+            ->setPath('/hello/Bob')
+            ->addHeader('Content-Type', 'application/json');
 
-	public static function setUpBeforeClass(): void
-	{
-		$config = new MockServerConfig();
-		// Configure the Pact mock server settings
-		$config->setHost('localhost');
-		$config->setPort(8000);
-		$config->setConsumer('Course');
-		$config->setProvider('User');
+        // Create your expected response from the provider.
+        $response = new ProviderResponse();
+        $response
+            ->setStatus(200)
+            ->addHeader('Content-Type', 'application/json')
+            ->setBody([
+                'message' => $matcher->term('Hello, Bob', '(Hello, )[A-Za-z]')
+            ]);
 
-		// Create and start the Pact mock server
-		self::$pactMockServer = new MockServer($config);
-		self::$pactMockServer->start();
-	}
+        // Create a configuration that reflects the server that was started. You can create a custom MockServerConfigInterface if needed.
+        $config  = new MockServerEnvConfig();
+        $builder = new InteractionBuilder($config);
+        $builder
+            ->uponReceiving('A get request to /hello/{name}')
+            ->with($request)
+            ->willRespondWith($response); // This has to be last. This is what makes an API request to the Mock Server to set the interaction.
 
-	public static function tearDownAfterClass(): void
-	{
-		// Stop the Pact mock server
-		self::$pactMockServer->stop();
-	}
-	public function testGetHelloString()
+        $service = new HttpClientService($config->getBaseUri()); // Pass in the URL to the Mock Server.
+        $result  = $service->getHelloString('Bob'); // Make the real API request against the Mock Server.
 
-	{
-		$matcher = new Matcher();
+        $builder->verify(); // This will verify that the interactions took place.
 
-		// Create your expected request from the consumer.
-		$request = new ConsumerRequest();
-		$request
-			->setMethod('GET')
-			->setPath('/api/course')
-			->addHeader('Content-Type', 'application/json');
-
-		// Create your expected response from the provider.
-		$response = new ProviderResponse();
-		$response
-			->setStatus(200)
-			->addHeader('Content-Type', 'application/json')
-			->setBody([
-				'list' => $matcher->eachLike([
-					'id' => 1,
-					'study_program_id' => 1,
-					'creator_user_id' => 1,
-					'name' => 'Pemrograman Web Lanjut',
-					'code' => 'A123',
-					'course_credit' => 3,
-					'lab_credit' => 1,
-					'type' => 'mandatory',
-					'short_description' => 'Pemrograman Web tingkat Lanjut',
-					'minimal_requirement' => 'Pemrograman Web',
-					'study_material' => 'laravel',
-					'learning_media' => 'slack',
-				])
-			]);
-
-		// Create a configuration that reflects the server that was started. You can create a custom MockServerConfigInterface if needed.
-		$config  = new MockServerEnvConfig();
-		$builder = new InteractionBuilder($config);
-		$builder
-			->uponReceiving('A Get request to /course')
-			->with($request)
-			->willRespondWith($response); // This has to be last. This is what makes an API request to the Mock Server to set the interaction.
-
-		$service = new HttpClientService($config->getBaseUri()); // Pass in the URL to the Mock Server.
-		$result  = $service->getHelloString('Bob'); // Make the real API request against the Mock Server.
-
-		$builder->verify(); // This will verify that the interactions took place.
-
-		$this->assertEquals('Succesfully create course', $result); // Make your assertions.
-	}
+        $this->assertEquals('Hello, Bob', $result); // Make your assertions.
+    }
 }
